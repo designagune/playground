0121bff42774228acd1710e5bfc14f3b88d15e12
designagune <yoon_seol@naver.com>
2020-10-29 15:45:36 +0900

    header add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect, useCallback } from 'react';
+import Header from './common/Header';
+import Main from './page/Main';
 function App() {
+
+  const [scrollStatus, setScrollStatus] = useState(false);
+  console.log("!");
+  const scrollToggle = useCallback (() => {
+    // window.scrollY >= 100 
+    // ? setScrollStatus(true)
+    // : setScrollStatus(false);
+
+    if(window.pageYOffset >= 100) {
+      setScrollStatus(true);
+    }else {
+      setScrollStatus(false);
+    }
+    console.log(window.scrollY ,scrollStatus);
+  }, [scrollStatus]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", scrollToggle )
+  }, [scrollStatus]);
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrap" style={{ height:"1500px" }}> {/*추후 스타일제거*/}
+      <Header/>
+      <Main status={ scrollStatus }/>
     </div>
   );
 }
